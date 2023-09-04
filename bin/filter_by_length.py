1cadc72785780c39b948d011ff85a9169a1ff618
limrp <limrod.15@gmail.com>
2023-09-04 13:32:11 -0500

    bin/filter_by_length.py reformatted with black.

--- bin/filter_by_length.py
+++ bin/filter_by_length.py
@@ -28,6 +28,7 @@ import argparse
 
 # *--------------------------------------* Defining functions *--------------------------------------------*
 
+
 # Function to read and filter sequences from a FASTA file or standard input
 def read_and_filter_fasta(file_object, threshold):
     seq_id = None
@@ -60,6 +61,7 @@ def read_and_filter_fasta(file_object, threshold):
 
 # *--------------------------------------* Primary logic of the script *------------------------------------*
 
+
 # Main function to parse command-line arguments and orchestrate reading, filtering, and writing sequences
 def main():
     parser = argparse.ArgumentParser(
@@ -69,16 +71,32 @@ def main():
         - With a file:
         python3 filter_seqs_by_length.py -i input.fasta -m 100 -o output.fasta
         - With standard input:
-        cat input.fasta | python3 filter_seqs_by_length.py -i - -m 100 -o output.fasta
-        pigz input.fasta.gz | python3 filter_seqs_by_length.py -i - -m 100 -o output.fasta
+        cat input.fasta | python3 filter_by_length.py -i - -m 100 -o out.fa
+        pigz input.fasta.gz | python3 filter_by_length.py -i - -m 100 -o.fa
         """,
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
-        "--input", "-i", type=argparse.FileType("r"), required=True, help="The input FASTA file or standard input."
+        "--input",
+        "-i",
+        type=argparse.FileType("r"),
+        required=True,
+        help="The input FASTA file or standard input.",
     )
-    parser.add_argument("--min_len", "-m", type=int, required=True, help="The minimum sequence length.")
-    parser.add_argument("--output", "-o", type=str, required=True, help="The output FASTA file.")
+    parser.add_argument(
+        "--min_len",
+        "-m",
+        type=int,
+        required=True,
+        help="The minimum sequence length.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        required=True,
+        help="The output FASTA file.",
+    )
     args = parser.parse_args()
 
     # Call the read_and_filter_fasta function and get a generator of sequences
